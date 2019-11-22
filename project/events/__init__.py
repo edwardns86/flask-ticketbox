f3cb7225b429f60023129cb1a271ee9e96abdaef
Edward Stevens <edwardstevens@Edwards-MacBook-Pro.local>
2019-11-22 10:00:46 +0700

    updated home route to redirect to login if not logged in'

--- project/events/__init__.py
+++ project/events/__init__.py
@@ -6,4 +6,6 @@ event_blueprint = Blueprint('events', __name__ ,template_folder='templates' )
 
 @event_blueprint.route('/home')
 def home():
+    if not current_user.is_authenticated:
+        return redirect(url_for('users.login'))
     return render_template('views/home.html')
